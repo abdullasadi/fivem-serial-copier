2b9e1d75e9c68961f9c16b558180f4a3ceb2d943
Kael <abdullasadi23@gmail.com>
2024-02-28 19:11:56 +0600

    update

--- sv_config.lua
+++ sv_config.lua
@@ -1,13 +1,13 @@
 CopySerialsHook = {
     Title = 'Weapon Serials',
     Name = 'kael-weapon-distributor',
-    Hook = "https://discord.com/api/webhooks/1032142194179584040/qpY7rjtl0tKfykSd_ec8ry6hIyVr0hFwjSpTEZAXxzrAY7goN12KaGToB_LhgkcUURfd",
-    Icon = "https://cdn.discordapp.com/attachments/965425749882437712/1198191814449049610/logo.jpg?ex=65be0215&is=65ab8d15&hm=db3d75149458eafc64aa90098f0812480da79613c9b584c073d523167a18a3b3&",
+    Hook = "",
+    Icon = "",
 }
 
 FindWeaponHook = {
     Title = 'Found Serial',
     Name = 'kael-weapon-distributor',
-    Hook = "https://discord.com/api/webhooks/1032142194179584040/qpY7rjtl0tKfykSd_ec8ry6hIyVr0hFwjSpTEZAXxzrAY7goN12KaGToB_LhgkcUURfd",
-    Icon = "https://cdn.discordapp.com/attachments/965425749882437712/1198191814449049610/logo.jpg?ex=65be0215&is=65ab8d15&hm=db3d75149458eafc64aa90098f0812480da79613c9b584c073d523167a18a3b3&",
+    Hook = "",
+    Icon = ",
 }
